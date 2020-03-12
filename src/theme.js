3082986d437e9001426cc841b34bd42baaaa4322
linekm475 <linus.ekman@utb.salem.se>
2020-03-12 21:14:06 +0100

    la till ljust läge

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,37 @@
-export default {
+export const darkMode = {
+  isDarkMode: true,
   colors: {
-    gray: [
-      "#F7FAFC",
-      "#EDF2F7",
-      "#E2E8F0",
-      "#CBD5E0",
-      "#A0AEC0",
-      "#718096",
-      "#4A5568",
-      "#2D3748",
-      "#1A202C"
-    ],
-    error: "#E53E3E"
+    error: "#E53E3E",
+    blue: "#2B6CB0",
+    bgMain: "#2D3748",
+    bgSecondary: "#1A202C",
+    bgTertiary: "rgba(0, 0, 0, 0.1)",
+    transparent: "rgba(0, 0, 0, 0)",
+    textMain: "#fff",
+    textSecondary: "#d1d2d5",
+    hover: "#10141a",
+    overlay: "rgba(0, 0, 0, 0.8)",
+  },
+  breakpoints: {
+    xs: "(max-width: 425px)",
+    sm: "(max-width: 768px)",
+    md: "(max-width: 950px)",
+  },
+};
+
+export const lightMode = {
+  isDarkMode: false,
+  colors: {
+    error: "#E53E3E",
+    blue: "#3182CE",
+    bgMain: "#E2E8F0",
+    bgSecondary: "#CBD5E0",
+    bgTertiary: "#d0d9e2",
+    transparent: "rgba(0, 0, 0, 0)",
+    textMain: "#000",
+    textSecondary: "#2D3748",
+    hover: "#b5c1ce", //F7FAFC"
+    overlay: "rgba(0, 0, 0, 0.8)",
   },
   breakpoints: {
     xs: "(max-width: 425px)",
